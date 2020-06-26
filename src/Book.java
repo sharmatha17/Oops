453d5501050cbe8cf82f2a8dd392977f0ede7bd0
sharmatha17 <60655169+sharmatha17@users.noreply.github.com>
2020-06-26 14:45:54 +0530

    Encapsulation/Abstraction

---
+++ src/Book.java
@@ -0,0 +1,41 @@
+public class Book extends Author
+{
+	String name;
+	Author author;
+	double price;
+	int qtyInStock;
+	public void Book(String name, Author author, double price, int qtyInStock) {
+		this.name = name;
+		this.author = author;
+		this.price = price;
+		this.qtyInStock = qtyInStock;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Author getAuthor() {
+		return author;
+	}
+	public void setAuthor(Author author) {
+		this.author = author;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public int getQtyInStock() {
+		return qtyInStock;
+	}
+	public void setQtyInStock(int qtyInStock) {
+		this.qtyInStock = qtyInStock;
+	}
+	@Override
+	public String toString() {
+		return "[name=" + name + ", email=" + email + ", gender=" + gender + ", price=" + price + ", qtyInStock=" + qtyInStock + "]";
+	}
+}
---
+++ src/Demo.java
@@ -0,0 +1,12 @@
+public class Demo {
+	public static void main(String[] args) 
+	{
+		Book b=new Book();
+		b.setName("shar");
+		b.email=("shar@gmail.com");
+		b.gender=('F');
+		b.setPrice(250.90);
+		b.setQtyInStock(10);
+		System.out.print(b);
+	}
+}
